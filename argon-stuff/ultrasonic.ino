95b1b53d06b99fc4ef38a6e6c26647af9de61744
djblazkowicz <56305919+djblazkowicz@users.noreply.github.com>
2020-10-28 13:22:15 +0000

    Update ultrasonic.ino

--- argon-stuff/ultrasonic.ino
+++ argon-stuff/ultrasonic.ino
@@ -1,32 +1,90 @@
-// This #include statement was automatically added by the Particle IDE.
 #include <HC-SR04.h>
-
 
 const int triggerPin = A0;
 const int echoPin = D0;
-double inch;
+const int HISTORY_LIMIT = 1000;
+const int SAMPLE_RATE = 100;
+const double TRESHOLD = 120.0;
+int counter = 0,inch_history_counter = 0, pothole_meter = 0;
+double inch,avg_inch = 0,inch_history[HISTORY_LIMIT],baseline;
+bool check_spike = false;
 HC_SR04 rangefinder = HC_SR04(triggerPin, echoPin);
-
-// We are going to declare a Particle.variable() here so that we can access the value of the photoresistor from the cloud.
-
-    // This is saying that when we ask the cloud for "analogvalue", this will reference the variable analogvalue in this app, which is an integer variable.
-
-    // We are also going to declare a Particle.function so that we can turn the LED on and off from the cloud.
-//Particle.function("led",ledToggle);
-    // This is saying that when we ask the cloud for the function "led", it will employ the function ledToggle() from this app.
 
 void setup()
 {
     Particle.variable("inch", inch);
     Serial.begin(9600);
     rangefinder.init();
+    baseline = get_average_inch();
+
 }
 
 void loop()
 {
-    unsigned long start = micros();
-    inch = rangefinder.distInch();
-    unsigned long calcTime = micros() - start;
-    Serial.printf("Range finding duration: %lu | Distance in inches: %.2f\n", calcTime, inch);
-    delay(10);
+    static int prev_ms = millis();
+    if ((millis() - prev_ms) > 1000 / SAMPLE_RATE)
+    {
+        inch = rangefinder.distInch();
+        check_spike = spike(baseline,inch,TRESHOLD);
+        if (check_spike)
+        {
+            pothole_meter++;
+            Serial.printf("POTHOLE METER: " + String(pothole_meter) + "\n");
+            
+        } else {
+            if (pothole_meter > 100) 
+            {
+                (Serial.printf("POTHOLE SIZE: " + String(pothole_meter) + "*******************************************************************************************************************\n"));
+                Particle.publish("my_event", String(pothole_meter), PRIVATE);
+                pothole_meter = 0;
+            } else
+            {
+                pothole_meter = 0;
+            }
+        }
+    }
+
+}
+
+
+double get_average_inch() {
+
+    double buffer = 0;
+    
+    for (int i = 0; i < HISTORY_LIMIT + 1; i++)
+    {
+            inch = rangefinder.distInch();
+            inch_history[inch_history_counter] = inch;
+            inch_history_counter++;
+            if (inch_history_counter > HISTORY_LIMIT - 1)
+            {
+                inch_history_counter = 0;
+                for (int i = 0; i < HISTORY_LIMIT + 1; i++)
+                {
+                    buffer = buffer + inch_history[i];
+                //Serial.printf("BUFFER: " + String(buffer) + "\n");
+                }
+            buffer = buffer / double(HISTORY_LIMIT);
+            //Serial.printf("DISTANCE: " + String(buffer) + "\n");
+            }
+
+    }
+    
+    
+    return buffer;
+}
+
+
+bool spike(double baseline, double current, double treshold) 
+{
+    double percent = (current / baseline) * 100;
+    //Serial.printf("PERCENT: " + String(percent) + "\n");
+    
+    if (percent > treshold)
+    {
+        return true;
+    }else {
+        return false;
+    }
+    
 }
